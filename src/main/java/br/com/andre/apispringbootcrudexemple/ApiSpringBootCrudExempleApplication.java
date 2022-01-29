851691ef943a7b61d7fdee157e342cec271f8d60
André Pereira <andre.pereira.fisica@gmail.com>
2022-01-29 14:28:06 -0300

    inicio da api

---
+++ src/main/java/br/com/andre/apispringbootcrudexemple/ApiSpringBootCrudExempleApplication.java
@@ -0,0 +1,13 @@
+package br.com.andre.apispringbootcrudexemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootCrudExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootCrudExempleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andre/apispringbootcrudexemple/ApiSpringBootCrudExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andre.apispringbootcrudexemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootCrudExempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
